6a1581f87cda68598c06c9e2fdf0d4d74590ccb4
Yuchun <yuchun.feng@mail.utoronto.ca>
2025-03-28 14:44:32 -0400

    ros synch

--- src/realsense_tracking/realsense_tracking/tracking_node.py
+++ src/realsense_tracking/realsense_tracking/tracking_node.py
@@ -2,22 +2,16 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
 from nav_msgs.msg import Odometry
-from collections import deque
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
 from rclpy.qos import qos_profile_system_default
+from message_filters import ApproximateTimeSynchronizer, Subscriber
 
 class T265Tracker(Node):
     def __init__(self):
         super().__init__('realsense_tracking')
         self.bridge = CvBridge()
-        
-        # Queues for image synchronization
-        self.left_queue = deque()
-        self.right_queue = deque()
-        self.left_info_queue = deque()
-        self.right_info_queue = deque()
         
         # Camera intrinsics
         self.K_left = self.D_left = self.P_left = None
@@ -25,103 +19,46 @@ class T265Tracker(Node):
         self.img_left = self.img_right = None
         self.pose = None
         
-        # Subscribe to Camera Info
-        self.create_subscription(CameraInfo, '/camera/fisheye1/camera_info', self.camera_info_callback_l, qos_profile_system_default)
-        self.create_subscription(CameraInfo, '/camera/fisheye2/camera_info', self.camera_info_callback_r, qos_profile_system_default)
+        # Create subscribers with message filters
+        self.left_info_sub = Subscriber(self, CameraInfo, '/camera/fisheye1/camera_info')
+        self.right_info_sub = Subscriber(self, CameraInfo, '/camera/fisheye2/camera_info')
+        self.left_image_sub = Subscriber(self, Image, '/camera/fisheye1/image_raw')
+        self.right_image_sub = Subscriber(self, Image, '/camera/fisheye2/image_raw')
+        self.pose_sub = Subscriber(self, Odometry, '/camera/pose/sample')
         
-        # Subscribe to Image Streams
-        self.create_subscription(Image, '/camera/fisheye1/image_raw', self.image_callback_l, qos_profile_system_default)
-        self.create_subscription(Image, '/camera/fisheye2/image_raw', self.image_callback_r, qos_profile_system_default)
+        # Synchronizer
+        self.sync = ApproximateTimeSynchronizer(
+            [self.left_info_sub, self.right_info_sub, self.left_image_sub, self.right_image_sub], 
+            queue_size=10, 
+            slop=0.05
+        )
+        self.sync.registerCallback(self.sync_callback)
         
-        # Subscribe to Camera Pose
-        self.create_subscription(Odometry, '/camera/pose/sample', self.pose_callback, qos_profile_system_default)
-        
-        # Publishers for rectified images and camera info
+        # Publishers
         self.left_image_pub = self.create_publisher(Image, '/left/image_raw', qos_profile_system_default)
         self.left_info_pub = self.create_publisher(CameraInfo, '/left/camera_info', qos_profile_system_default)
         self.right_image_pub = self.create_publisher(Image, '/right/image_raw', qos_profile_system_default)
         self.right_info_pub = self.create_publisher(CameraInfo, '/right/camera_info', qos_profile_system_default)
 
-    def camera_info_callback_l(self, msg):
-        """Store left camera info and check for synchronization."""
-        self.K_left = np.array(msg.k).reshape(3, 3)
-        self.D_left = np.array(msg.d)
-        self.P_left = np.array(msg.p).reshape(3, 4)
-        self.left_info_queue.append(msg)
-        self.process_synchronized_images()
-
-    def camera_info_callback_r(self, msg):
-        """Store right camera info and check for synchronization."""
-        self.K_right = np.array(msg.k).reshape(3, 3)
-        self.D_right = np.array(msg.d)
-        self.P_right = np.array(msg.p).reshape(3, 4)
-        self.right_info_queue.append(msg)
-        self.process_synchronized_images()
-
-    def image_callback_l(self, msg):
-        """Store left image and check for synchronization."""
-        self.left_queue.append(msg)
-        self.process_synchronized_images()
-
-    def image_callback_r(self, msg):
-        """Store right image and check for synchronization."""
-        self.right_queue.append(msg)
-        self.process_synchronized_images()
-
-    def process_synchronized_images(self):
-        """Match left and right images along with their camera info based on timestamp."""
-        if not self.left_queue or not self.right_queue or not self.left_info_queue or not self.right_info_queue:
-            return  # Wait for all queues to have messages
-
-        left_msg = self.left_queue[0]
-        right_msg = self.right_queue[0]
-        left_info_msg = self.left_info_queue[0]
-        right_info_msg = self.right_info_queue[0]
-
-        time_diff_img = abs(left_msg.header.stamp.sec + left_msg.header.stamp.nanosec * 1e-9 -
-                            right_msg.header.stamp.sec - right_msg.header.stamp.nanosec * 1e-9)
-
-        time_diff_info = abs(left_info_msg.header.stamp.sec + left_info_msg.header.stamp.nanosec * 1e-9 -
-                              right_info_msg.header.stamp.sec - right_info_msg.header.stamp.nanosec * 1e-9)
-        time_diff_ci = abs(left_info_msg.header.stamp.sec + left_info_msg.header.stamp.nanosec * 1e-9 -
-                              left_msg.header.stamp.sec - left_msg.header.stamp.nanosec * 1e-9)
-        if time_diff_img < 0.01 and time_diff_info < 0.01 and time_diff_ci < 0.01 : 
-            self.left_info_queue.popleft()
-            self.right_info_queue.popleft()
-            self.left_queue.popleft()
-            self.right_queue.popleft()
-
-            self.img_left = self.bridge.imgmsg_to_cv2(left_msg, desired_encoding='mono8')
-            self.img_right = self.bridge.imgmsg_to_cv2(right_msg, desired_encoding='mono8')
-            
-            self.get_logger().info(f"Synchronized images and info at {left_msg.header.stamp.sec}.{right_msg.header.stamp.sec}")
-            
-            # Publish synchronized images and camera info
-            self.left_image_pub.publish(left_msg)
-            self.right_image_pub.publish(right_msg)
-            self.left_info_pub.publish(left_info_msg)
-            self.right_info_pub.publish(right_info_msg)
-
-        elif left_info_msg.header.stamp.sec < right_info_msg.header.stamp.sec:
-            self.left_info_queue.popleft()
-        elif left_info_msg.header.stamp.sec > right_info_msg.header.stamp.sec:
-            self.right_info_queue.popleft()
-
-        elif left_msg.header.stamp.sec < right_msg.header.stamp.sec:
-            self.left_queue.popleft()  # Drop old left image
-        elif left_msg.header.stamp.sec > right_msg.header.stamp.sec:
-            self.right_queue.popleft()  # Drop old right image
+    def sync_callback(self, left_info, right_info, left_img, right_img):
+        """Processes synchronized image and camera info messages."""
+        self.K_left = np.array(left_info.k).reshape(3, 3)
+        self.D_left = np.array(left_info.d)
+        self.P_left = np.array(left_info.p).reshape(3, 4)
         
-        # now lc-rc and li-ri are synchd
-        #next we synch rc-ri
+        self.K_right = np.array(right_info.k).reshape(3, 3)
+        self.D_right = np.array(right_info.d)
+        self.P_right = np.array(right_info.p).reshape(3, 4)
         
-        elif left_info_msg.header.stamp.sec < left_msg.header.stamp.sec:
-            self.left_info_queue.popleft()
-            self.right_info_queue.popleft()
-
-        elif left_info_msg.header.stamp.sec > left_msg.header.stamp.sec:
-            self.left_queue.popleft() 
-            self.right_queue.popleft() 
+        self.img_left = self.bridge.imgmsg_to_cv2(left_img, desired_encoding='mono8')
+        self.img_right = self.bridge.imgmsg_to_cv2(right_img, desired_encoding='mono8')
+        
+        self.get_logger().info(f"Synchronized images at {left_img.header.stamp.sec}.{right_img.header.stamp.sec}")
+        
+        self.left_image_pub.publish(left_img)
+        self.right_image_pub.publish(right_img)
+        self.left_info_pub.publish(left_info)
+        self.right_info_pub.publish(right_info)
 
     def pose_callback(self, msg):
         """Extract camera position & orientation in world frame."""
