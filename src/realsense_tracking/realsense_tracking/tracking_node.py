d2e126a7876b6167168136bde95a606e3818dc56
Yuchun <yuchun.feng@mail.utoronto.ca>
2025-03-28 23:03:47 -0400

    distortion

--- src/realsense_tracking/realsense_tracking/tracking_node.py
+++ src/realsense_tracking/realsense_tracking/tracking_node.py
@@ -8,10 +8,29 @@ from cv_bridge import CvBridge
 from rclpy.qos import qos_profile_system_default
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 
+
+# stereo camera constants
+H, W = 800, 848
+IMG_SIZE_WH = (W, H)
+DOWNSCALE_H = 8
+STEREO_SIZE_WH = (W, H//DOWNSCALE_H)
+BASELINE = -18.2928466796875/286.1825866699219 # 64 mm baseline
+DROP_FRAMES = 3
+
+MAPX1 = None
+MAPY1 = None
+MAPX2 = None
+MAPY2 = None
+
+CAM_INFO1_ORIGINAL = None
+CAM_INFO2_ORIGINAL = None
+CAM_INFO1_MODIFIED = None
+CAM_INFO2_MODIFIED = None
+BRIDGE = CvBridge()
+
 class T265Tracker(Node):
     def __init__(self):
         super().__init__('realsense_tracking')
-        self.bridge = CvBridge()
         
         # Camera intrinsics
         self.K_left = self.D_left = self.P_left = None
@@ -28,10 +47,10 @@ class T265Tracker(Node):
         
         # Synchronizer
         self.sync = ApproximateTimeSynchronizer(
-            [self.left_info_sub, self.right_info_sub, self.left_image_sub, self.right_image_sub], 
-            queue_size=10, 
-            slop=0.05
+            [self.left_image_sub, self.right_image_sub, self.left_info_sub, self.right_info_sub], 
+            10
         )
+
         self.sync.registerCallback(self.sync_callback)
         
         # Publishers
@@ -40,7 +59,7 @@ class T265Tracker(Node):
         self.right_image_pub = self.create_publisher(Image, '/right/image_raw', qos_profile_system_default)
         self.right_info_pub = self.create_publisher(CameraInfo, '/right/camera_info', qos_profile_system_default)
 
-    def sync_callback(self, left_info, right_info, left_img, right_img):
+    def sync_callback0(self, left_info, right_info, left_img, right_img):
         """Processes synchronized image and camera info messages."""
         self.K_left = list(left_info.k) #np.array(left_info.k).reshape(3, 3)
         self.D_left = list(left_info.d) #np.array(left_info.d)
@@ -60,6 +79,103 @@ class T265Tracker(Node):
         self.right_image_pub.publish(right_img)
         self.left_info_pub.publish(left_info)
         self.right_info_pub.publish(right_info)
+
+    def sync_callback(self, img_msg1, img_msg2, camera_info_msg1, camera_info_msg2):
+        global DROP_IND
+        check_none = (
+            self.left_image_pub,
+            self.right_image_pub,
+            self.left_info_pub,
+            self.right_info_pub,
+        )
+        if any([x is None for x in check_none]):
+            self.get_logger().warn("Waiting for camera info")
+            return
+        if any([x is None for x in (MAPX1, MAPY1, MAPX2, MAPY2)]):
+            self.init_maps(camera_info_msg1, camera_info_msg2)
+        # drop frames to reduce load
+        DROP_IND += 1
+        if DROP_IND < DROP_FRAMES:
+            return
+        DROP_IND = 0
+
+        img_distorted1 = BRIDGE.imgmsg_to_cv2(img_msg1, desired_encoding="passthrough")
+        img_undistorted1 = cv2.remap(
+            img_distorted1,
+            MAPX1,
+            MAPY1,
+            interpolation=cv2.INTER_LINEAR,
+            borderMode=cv2.BORDER_CONSTANT,
+        )
+        img_distorted2 = BRIDGE.imgmsg_to_cv2(img_msg2, desired_encoding="passthrough")
+        img_undistorted2 = cv2.remap(
+            img_distorted2,
+            MAPX2,
+            MAPY2,
+            interpolation=cv2.INTER_LINEAR,
+            borderMode=cv2.BORDER_CONSTANT,
+        )
+        # crop top and bottom based on DOWNSCALE_H
+        orig_height = img_undistorted1.shape[0]
+        new_height = orig_height//DOWNSCALE_H
+
+        # take center of image of new height
+        img_undistorted1 = img_undistorted1[
+            (orig_height - new_height)//2 : (orig_height + new_height)//2, :
+        ]
+        img_undistorted2 = img_undistorted2[
+            (orig_height - new_height)//2 : (orig_height + new_height)//2, :
+        ]
+        
+        # convert from mono8 to bgr8
+        img_undistorted1 = cv2.cvtColor(img_undistorted1, cv2.COLOR_GRAY2BGR)
+        output_msg1 = BRIDGE.cv2_to_imgmsg(img_undistorted1, encoding="bgr8")
+        output_msg1.header = img_msg1.header
+        img_undistorted2 = cv2.cvtColor(img_undistorted2, cv2.COLOR_GRAY2BGR)
+        output_msg2 = BRIDGE.cv2_to_imgmsg(img_undistorted2, encoding="bgr8")
+        output_msg2.header = img_msg2.header
+
+        # update camera info
+        camera_info_msg1 = self.modify_camera_info(camera_info_msg1)
+        camera_info_msg2 = self.modify_camera_info(camera_info_msg2)
+
+        # publish
+        self.left_image_pub.publish(output_msg1)
+        self.right_image_pub.publish(output_msg2)
+        self.left_info_pub.publish(camera_info_msg1)
+        self.right_info_pub.publish(camera_info_msg2)
+
+
+    def modify_camera_info(self, msg):
+        msg.distortion_model = "plumb_bob"
+        msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
+        # downscale K and P
+        msg.K = list(msg.K)
+        msg.K[5] = msg.K[5]/DOWNSCALE_H # optical center
+        msg.P = list(msg.P)
+        msg.P[6] = msg.P[6]/DOWNSCALE_H # optical center
+        msg.height = msg.height//DOWNSCALE_H
+        return msg
+
+
+    def init_maps(self, cam_info1, cam_info2):
+        global MAPX1, MAPY1, MAPX2, MAPY2
+        K1 = np.array(cam_info1.K).reshape(3,3)
+        D1 = np.array(cam_info1.D)
+        K2 = np.array(cam_info2.K).reshape(3,3)
+        D2 = np.array(cam_info2.D)
+        T = np.array([BASELINE, 0, 0]) # 64 mm baseline
+
+        R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(
+            K1, D1, K2, D2, IMG_SIZE_WH, R=np.eye(3), T=T
+        )
+        MAPX1, MAPY1 = cv2.fisheye.initUndistortRectifyMap(
+            K1, D1, R1, P1, size=IMG_SIZE_WH, m1type=cv2.CV_32FC1
+        )
+        MAPX2, MAPY2 = cv2.fisheye.initUndistortRectifyMap(
+            K2, D2, R2, P2, size=IMG_SIZE_WH, m1type=cv2.CV_32FC1
+        )
+
 
     def pose_callback(self, msg):
         """Extract camera position & orientation in world frame."""
