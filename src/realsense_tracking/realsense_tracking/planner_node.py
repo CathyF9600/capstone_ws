151e323bed02627e61dd230bacb87977adbac3a8
Cathy Feng <cathyfeng9600@gmail.com>
2025-04-10 19:14:20 -0400

    Update planner_node.py

--- src/realsense_tracking/realsense_tracking/planner_node.py
+++ src/realsense_tracking/realsense_tracking/planner_node.py
@@ -170,14 +170,20 @@ class RGBDPathPlanner(Node):
 
         return [pcd, occupancy, sphere, axis, line_path]
 
+def set_fixed_view(vis):
+    ctr = vis.get_view_control()
+    # Set the camera parameters (position, look-at, up vector, field of view)
+    ctr.set_lookat([0, 0, 0])  # Where the camera is looking at (origin in this case)
+    ctr.set_up([0, 0, 1])      # The up vector (Z-axis is up)
+    ctr.set_front([0, -1, -1]) # Front vector (camera direction)
+    ctr.set_zoom(0.5)
+    
 def main(args=None):
     rclpy.init(args=args)
 
     vis = o3d.visualization.Visualizer()
     vis.create_window("RGBD Path Planner", 800, 600)
-    ctr = vis.get_view_control()
-    ctr.set_zoom(0.5)  # Zoom in (lower is more zoomed)
-    ctr.rotate(0, -45)  # Rotate to look down ~45 deg
+    set_fixed_view(vis)
     geometry_queue = queue.Queue()
 
     node = RGBDPathPlanner(vis, geometry_queue)
